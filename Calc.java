58670313fa397004abc4a85aad3b8102d9dbd486
EC2 Default User <ec2-user@ip-172-31-55-23.ec2.internal>
2023-08-19 06:25:22 +0000

    first java code

--- Calc.java
+++
@@ -1,9 +0,0 @@
-class Calc
-{
-  public static void main(String args[])
-  {
-        int a=20;
-        int b=20;
-        int c = a+b;
-        System.out.println("Addition of two numbers"a+" & "+b" is ",+c);
-  }
