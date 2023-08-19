67df227a62895d83a0e42c75115b03fa965532e1
Rishi91379 <141408958+Rishi91379@users.noreply.github.com>
2023-08-19 11:39:16 +0530

    Create Calc.java
    Simple Java program to perform addition of two numbers

---
+++ Calc.java
@@ -0,0 +1,9 @@
+class Calc
+{
+  public static void main(String args[])
+  {
+        int a=20;
+        int b=20;
+        int c = a+b;
+        System.out.println("Addition of two numbers"a+" & "+b" is ",+c);
+  }
